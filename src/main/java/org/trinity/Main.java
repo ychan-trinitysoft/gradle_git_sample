9b1207b4c48d8e7d2d419aa9575cea186fcf7d2c
ychan-trinitysoft <ychan@trinitysoft.co.kr>
2025-02-14 14:27:10 +0900

    Removed deleted files

--- src/main/java/org/trinity/Main.java
+++ src/main/java/org/trinity/Main.java
@@ -1,9 +1,31 @@
+/*
+ * gradle_git_sample - digital logic design tool and simulator Copyright by the gradle_git_sample developers
+ *
+ * https://github.com/ychan-trinitysoft/
+ *
+ * This is free software released under GNU GPLv3 license
+ */
+
 package org.trinity;
+
+import org.jdesktop.swingx.action.AbstractActionExt;
+import org.fife.io.DocumentReader;
+import com.formdev.flatlaf.FlatClientProperties;
+import org.apache.commons.cli.AlreadySelectedException;
+import javax.help.AbstractHelpAction;
+import net.sf.nimrod.NimRODApplet;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        AbstractActionExt abstractActionExt;
+        DocumentReader documentReader;
+        FlatClientProperties flatClientProperties;
+        AlreadySelectedException alreadySelectedException;
+        AbstractHelpAction abstractHelpAction;
+        NimRODApplet nimRODApplet;
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
